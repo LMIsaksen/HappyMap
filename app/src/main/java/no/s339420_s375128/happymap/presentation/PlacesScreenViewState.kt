58d5d7219be6442f7e822d63df3ac7a0764240ac
Caroline Moland Naterstad <103410211+camona@users.noreply.github.com>
2024-11-21 16:38:49 +0100

    Prøve å få inn Google Maps

--- app/src/main/java/no/s339420_s375128/happymap/presentation/PlacesScreenViewState.kt
+++ app/src/main/java/no/s339420_s375128/happymap/presentation/PlacesScreenViewState.kt
@@ -1,2 +1,18 @@
 package no.s339420_s375128.happymap.presentation
 
+import com.google.android.gms.maps.model.LatLngBounds
+import no.s339420_s375128.happymap.data.Place
+
+sealed class PlacesScreenViewState {
+    data object Loading : PlacesScreenViewState()
+    data class PlaceList(
+        // List of the mountains to display
+        val mountains: List<Place>,
+
+        // Bounding box that contains all of the mountains
+        val boundingBox: LatLngBounds,
+
+        // Switch indicating whether all the mountains or just the 14ers
+        val showingAllPeaks: Boolean = false,
+    ) : PlacesScreenViewState()
+}
